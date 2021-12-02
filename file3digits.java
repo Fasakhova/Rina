99c71cab13baa4b6756becbc58950be392e622dc
Fasakhova <90395046+Fasakhova@users.noreply.github.com>
2021-12-02 21:31:10 +0300

    Update file3digits.java

--- file3digits.java
+++ file3digits.java
@@ -1,8 +1,8 @@
 public class file3digits {
         public static void main(String[] args) {
-            System.out.println(introTask3Digits(34536));
-            System.out.println(introTask3Digits(97));
-            System.out.println(introTask3Digits(827));
+            System.out.println(introTask3Digits(54445;
+            System.out.println(introTask3Digits(900);
+            System.out.println(introTask3Digits(29);
         }
 
         private static boolean introTask3Digits(int x) {
